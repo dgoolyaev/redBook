ed3ffb01505e18ffa337906c2839c51191627d79
dgoolyaev <workDmitriyGulyaev@yandex.ru>
2025-04-25 03:45:01 +0300

    plus exrss

---
+++ src/main/scala/exercises/ExerciseTwoDotTwoPageTwentyFour.scala
@@ -0,0 +1,22 @@
+package exercises
+
+/**
+ * Implement isSorted, which checks whether an Array[A] is sorted according to a
+ * given comparison function:
+ * def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean
+ *
+ * I don't understand this shit completely, I guess I must use function `ordered` with n
+ * and n + 1 elements, but I'm not sure
+ */
+
+object ExerciseTwoDotTwoPageTwentyFour {
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(n: Int): Boolean = {
+      if (n == as.length) true
+      else if (!ordered(as(n), as(n+1))) false
+      else loop(n+1)
+    }
+    loop(0)
+  }
+}
