0c4c11e771a8124e55e34e5c23942aec65f4fb5a
dgoolyaev <workDmitriyGulyaev@yandex.ru>
2025-04-25 03:48:05 +0300

    plus exrss

--- src/main/scala/exercises/ExerciseTwoDotTwoPageTwentyFour.scala
+++ src/main/scala/exercises/ExerciseTwoDotTwoPageTwentyFour.scala
@@ -13,7 +13,7 @@ object ExerciseTwoDotTwoPageTwentyFour {
   def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
     @annotation.tailrec
     def loop(n: Int): Boolean = {
-      if (n == as.length) true
+      if (n > as.length) true
       else if (!ordered(as(n), as(n+1))) false
       else loop(n+1)
     }
