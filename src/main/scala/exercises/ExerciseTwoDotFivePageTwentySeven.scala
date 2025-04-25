47ae8be4211c5722562db610acc528b2142e8602
dgoolyaev <gulyaev4237@gmail.com>
2025-04-25 10:27:07 +0300

    kk

--- src/main/scala/exercises/ExerciseTwoDotFivePageTwentySeven.scala
+++ src/main/scala/exercises/ExerciseTwoDotFivePageTwentySeven.scala
@@ -5,6 +5,12 @@ package exercises
  * def compose[A,B,C](f: B => C, g: A => B): A => C
  */
 
-object ExerciseTwoDotFivePageTwentySeven {
+object ExerciseTwoDotFivePageTwentySeven extends App {
   def compose[A,B,C](f: B => C, g: A => B): A => C = a => f(g(a))
+
+  val incr = (x: Int) => x + 1
+
+  val mult = (x: Int) => x * 2
+
+  println(compose(mult, incr)(5))
 }
