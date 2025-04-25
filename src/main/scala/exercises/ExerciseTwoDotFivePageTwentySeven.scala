7b02b9cf4b2308ffcf7ab3050e01aca517307ced
dgoolyaev <gulyaev4237@gmail.com>
2025-04-25 08:59:19 +0300

    ok understood

---
+++ src/main/scala/exercises/ExerciseTwoDotFivePageTwentySeven.scala
@@ -0,0 +1,10 @@
+package exercises
+
+/**
+ *Implement the higher-order function that composes two functions.
+ * def compose[A,B,C](f: B => C, g: A => B): A => C
+ */
+
+object ExerciseTwoDotFivePageTwentySeven {
+  def compose[A,B,C](f: B => C, g: A => B): A => C = g(a) => f(b)
+}
