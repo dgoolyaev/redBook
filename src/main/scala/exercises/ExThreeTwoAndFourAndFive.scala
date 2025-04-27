a68ba93e5ec203326e09f1c148fce32ed135d265
dgoolyaev <gulyaev4237@gmail.com>
2025-04-27 05:44:06 +0300

    kk

--- src/main/scala/exercises/ExThreeTwoAndFourAndFive.scala
+++ src/main/scala/exercises/ExThreeTwoAndFourAndFive.scala
@@ -7,7 +7,13 @@ package exercises
  * def drop[A](l: List[A], n: Int): List[A]
  */
 
-object ExThreeFour extends App{
+/**
+ * Implement dropWhile, which removes elements from the List prefix as long as they
+ * match a predicate.
+ * def dropWhile[A](l: List[A], f: A => Boolean): List[A]
+ */
+
+object ExThreeTwoAndFourAndFive extends App{
   sealed trait List[+A]
   case object Nil extends List[Nothing]
   case class Cons[+A](head: A, tail: List[A]) extends List[A]
@@ -26,8 +32,20 @@ object ExThreeFour extends App{
       }
       loop(0, l)
     }
+
+    @annotation.tailrec
+    def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
+      l match {
+        case Nil => Nil
+        case Cons(head, tail) =>
+          if f(head) then dropWhile(tail, f)
+          else l
+      }
+    }
   }
 
   val example: List[Int] = Cons(1, Cons(2, Cons(3, Cons(4, Cons(5, Nil)))))
   println(List.drop(example, 3))
+
+  println(List.dropWhile(example, (x: Int) => x < 3))
 }
